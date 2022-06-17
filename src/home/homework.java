ecf9e2edbb6fe729809a9eef22d1d55566e36ecf
Miki <miik04i@gmail.com>
2022-06-17 11:56:21 +0200

    test

--- src/home/homework.java
+++ src/home/homework.java
@@ -4,6 +4,6 @@ public class homework {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        System.out.println("testis...");
+        System.out.println("test...");
     }
 }
